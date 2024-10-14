f5d9dcf16ad2b316aae227e7432d1ae91fa1f2a9
Sunnygame06 <20210133@ricaldone.edu.sv>
2024-10-14 16:46:00 -0600

    Ultimos cambios 2

--- Controlador/Servidor/ControllerAdminconexcion.cs
+++ Controlador/Servidor/ControllerAdminconexcion.cs
@@ -69,70 +69,63 @@ namespace Login_Farmacia.Controlador.Servidor
 
         public void GuardarConfiguracionXML()
         {
-            if (ObjConn.txtUser.Text == "" || ObjConn.txtDataBase.Text == "" || ObjConn.txtSqlAuth.Text == "" || ObjConn.txtSqlPass.Text == "")
+            try
             {
-                MessageBox.Show("EC02-Algunos campos no se han llenado", "Accion Interrumpida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                XmlDocument doc = new XmlDocument();
+
+                XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                doc.AppendChild(decl);
+
+                XmlElement root = doc.CreateElement("Conn");
+                doc.AppendChild(root);
+
+                XmlElement servidor = doc.CreateElement("Server");
+                string servidorCode = CodificarBase64String(ObjConn.txtUser.Text.Trim());
+                servidor.InnerText = servidorCode;
+                root.AppendChild(servidor);
+
+                XmlElement DataBase = doc.CreateElement("Database");
+                string DataBaseCode = CodificarBase64String(ObjConn.txtDataBase.Text.Trim());
+                DataBase.InnerText = DataBaseCode;
+                root.AppendChild(DataBase);
+
+                if (ObjConn.rdDeshabilitarWindows.Checked == true)
+                {
+                    XmlElement SqlAuth = doc.CreateElement("SqlAuth");
+                    string SqlAuthCode = CodificarBase64String(ObjConn.txtSqlAuth.Text.Trim());
+                    SqlAuth.InnerText = SqlAuthCode;
+                    root.AppendChild(SqlAuth);
+
+                    XmlElement SqlPass = doc.CreateElement("SqlPass");
+                    string SqlPassCode = CodificarBase64String(ObjConn.txtSqlPass.Text.Trim());
+                    SqlPass.InnerText = SqlPassCode;
+                    root.AppendChild(SqlPass);
+                }
+                else
+                {
+                    XmlElement SqlAuth = doc.CreateElement("SqlAuth");
+                    SqlAuth.InnerText = string.Empty;
+                    root.AppendChild(SqlAuth);
+
+                    XmlElement SqlPass = doc.CreateElement("SqlPass");
+                    SqlPass.InnerText = string.Empty;
+                    root.AppendChild(SqlPass);
+                }
+                SqlConnection con = dbContext.testConnection(ObjConn.txtUser.Text.Trim(), ObjConn.txtDataBase.Text.Trim(), ObjConn.txtSqlAuth.Text.Trim(), ObjConn.txtSqlPass.Text.Trim());
+                if (con != null)
+                {
+                    doc.Save("config_server.xml");
+                    DTOdbContext.Server = ObjConn.txtUser.Text.Trim();
+                    DTOdbContext.Database = ObjConn.txtDataBase.Text.Trim();
+                    DTOdbContext.User = ObjConn.txtSqlAuth.Text.Trim();
+                    DTOdbContext.Password = ObjConn.txtSqlPass.Text.Trim();
+                    MessageBox.Show($"El archivo fue creado exitosamente", "Archivo de configuración", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ObjConn.Hide();
+                }
             }
-            else
+            catch (XmlException ex)
             {
-                try
-                {
-                    XmlDocument doc = new XmlDocument();
-
-                    XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-                    doc.AppendChild(decl);
-
-                    XmlElement root = doc.CreateElement("Conn");
-                    doc.AppendChild(root);
-
-                    XmlElement servidor = doc.CreateElement("Server");
-                    string servidorCode = CodificarBase64String(ObjConn.txtUser.Text.Trim());
-                    servidor.InnerText = servidorCode;
-                    root.AppendChild(servidor);
-
-                    XmlElement DataBase = doc.CreateElement("Database");
-                    string DataBaseCode = CodificarBase64String(ObjConn.txtDataBase.Text.Trim());
-                    DataBase.InnerText = DataBaseCode;
-                    root.AppendChild(DataBase);
-
-                    if (ObjConn.rdDeshabilitarWindows.Checked == true)
-                    {
-                        XmlElement SqlAuth = doc.CreateElement("SqlAuth");
-                        string SqlAuthCode = CodificarBase64String(ObjConn.txtSqlAuth.Text.Trim());
-                        SqlAuth.InnerText = SqlAuthCode;
-                        root.AppendChild(SqlAuth);
-
-                        XmlElement SqlPass = doc.CreateElement("SqlPass");
-                        string SqlPassCode = CodificarBase64String(ObjConn.txtSqlPass.Text.Trim());
-                        SqlPass.InnerText = SqlPassCode;
-                        root.AppendChild(SqlPass);
-                    }
-                    else
-                    {
-                        XmlElement SqlAuth = doc.CreateElement("SqlAuth");
-                        SqlAuth.InnerText = string.Empty;
-                        root.AppendChild(SqlAuth);
-
-                        XmlElement SqlPass = doc.CreateElement("SqlPass");
-                        SqlPass.InnerText = string.Empty;
-                        root.AppendChild(SqlPass);
-                    }
-                    SqlConnection con = dbContext.testConnection(ObjConn.txtUser.Text.Trim(), ObjConn.txtDataBase.Text.Trim(), ObjConn.txtSqlAuth.Text.Trim(), ObjConn.txtSqlPass.Text.Trim());
-                    if (con != null)
-                    {
-                        doc.Save("config_server.xml");
-                        DTOdbContext.Server = ObjConn.txtUser.Text.Trim();
-                        DTOdbContext.Database = ObjConn.txtDataBase.Text.Trim();
-                        DTOdbContext.User = ObjConn.txtSqlAuth.Text.Trim();
-                        DTOdbContext.Password = ObjConn.txtSqlPass.Text.Trim();
-                        MessageBox.Show($"El archivo fue creado exitosamente", "Archivo de configuración", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        ObjConn.Hide();
-                    }
-                }
-                catch (XmlException ex)
-                {
-                    MessageBox.Show($"{ex.Message}, no se pudo crear el archivo de configuración.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                MessageBox.Show($"{ex.Message}, no se pudo crear el archivo de configuración.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
